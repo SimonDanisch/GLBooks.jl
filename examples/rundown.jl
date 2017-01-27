b29cfcecea6d71cfbfe3180caf454c10c8d6e1ac
Simon Danisch <sdanisch@gmail.com>
2017-01-27 16:38:11 -0500

    sanitize the blocks better

---
+++ examples/rundown.jl
@@ -0,0 +1,28 @@
+using GLVisualize, FileIO, Colors
+using GLBooks, Reactive
+
+GLBooks.init()
+dir = homedir()*"/Desktop/lesson/"
+
+surf(i, N) = Float32[sin(x*i) * cos(y*i) for x = linspace(0, 2pi, N), y = linspace(0, 2pi, N)]
+
+@block "Overview" begin
+    load(dir*"overview.png")
+end
+
+@block "Surface" begin
+    s1 = slider(linspace(0.1f0, 2pi, 100), "surface function")
+    drawpage(map(x-> surf(x, 128), s1), :surface)
+end
+
+
+@block "Surface with Image" begin
+    video = load(dir * "funky.gif")
+    play_s = playbutton("play")
+    idx = foldp(1, fpswhen(play_s, 30.0)) do v0, t
+        mod1(v0 + 1, size(video, 3))
+    end
+    imstream = map(i-> video[:, :, i], idx)
+    s1 = slider(linspace(0.5f0, 2pi, 100), "surface function")
+    drawpage(map(x-> surf(x, 128), s1), :surface, color = imstream)
+end
--- src/GLBooks.jl
+++ src/GLBooks.jl
@@ -2,25 +2,14 @@
 __precompile__(false)
 module GLBooks
 
-using Colors, Images, Reactive, GeometryTypes, GLAbstraction, GLWindow
+using Colors, Images, Reactive, GeometryTypes, GLAbstraction, GLWindow, FileIO
 import GLVisualize
 import GLVisualize: mm, layoutscreens, IRect, _view, visualize, glscreen
 import GLVisualize: x_partition_abs, loadasset
 import GLWindow: hide!, show!
 
-const blocklist = Screen[]
-const current_edit_screen = Ref{Screen}()
-const current_view_screen = Ref{Screen}()
-const current_widgetlist = Pair[]
-edit_screen() = current_edit_screen[]
-view_screen() = current_view_screen[]
-widgetlist() = current_widgetlist
-const _icon_size = Signal(10mm)
+const _icon_size = Signal(9mm)
 icon_size() = value(_icon_size)
-empty!(blocklist)
-
-
-parent_screen = glscreen(); @async GLWindow.waiting_renderloop(parent_screen)
 
 function play_controls(screen)
     # load the icons
@@ -28,6 +17,7 @@ function play_controls(screen)
         "rewind_inactive.png", "rewind_active.png",
         "back_inactive.png", "back_active.png",
     ]
+
     imgs = map(paths) do path
         img = map(RGBA{U8}, loadasset(path))
         img, flipdim(img, 1)
@@ -47,53 +37,90 @@ function play_controls(screen)
     visual, signals
 end
 
-navigation_visual, signals = play_controls(parent_screen)
-_position = 1
-for (i, block) in enumerate(blocklist)
-    _position == i ? show!(block) : hide!(block)
-end
+global edit_screen, view_screen, widgetlist, new_block, init
+
+global _block_position = 0
+const navigation = []
+const parent_screen = Ref{Screen}()
+const blocklist = Screen[]
+const current_widgetlist = []
+
+edit_screen() = blocklist[_block_position].children[1]
+view_screen() = blocklist[_block_position].children[2]
+widgetlist() = current_widgetlist[_block_position]
+
 step_page(dir) = max(mod1(dir, length(blocklist)), 1)
-isdefined(:control_s) && foreach(close, control_s)
-control_s = map(zip(signals, [-1, -1, 1, 1])) do sdir
-    global _position
-    s, dir = sdir
-    map(s) do s
-        if s && !isempty(blocklist)
-            for (i, block) in enumerate(blocklist)
-                _position == i ? show!(block) : hide!(block)
-            end
-            _position = step_page(_position + dir)
-        else
-            _position
+
+function handle_drop(files::Vector{String})
+    for f in files
+        try
+            drawpage(load(f))
+        catch e
+            warn(e)
         end
     end
 end
 
-function new_block()
-    parent = Screen(parent_screen, hidden = Signal(false)) # copy screen, to make it easier to save!
-    editarea, viewarea = x_partition_abs(parent.area, round(Int, 8.2 * icon_size()))
-    edit_screen = Screen(
-        parent, area = editarea,
-    )
-    view_screen = Screen(
-        parent, area = viewarea, name = :GLBook
-    )
-    GLVisualize.add_screen(view_screen)
-    edit_screen.stroke = (1, RGBA(0.9f0, 0.9f0, 0.9f0))
-    current_edit_screen[] = edit_screen
-    current_view_screen[] = view_screen
+
+function init()
+    global _block_position
+    _block_position = 0
+    parent_screen[] = glscreen()
+    @async GLWindow.renderloop(parent_screen[])
+    empty!(blocklist)
+    for list in current_widgetlist
+        for (nameobj, s) in list
+            close(s, false)
+        end
+    end
     empty!(current_widgetlist)
+    if !isempty(navigation) # old signals in there
+        for s in navigation[end]
+            close(s, false) # close old signal
+        end
+        empty!(navigation)
+    end
+    push!(navigation, play_controls(parent_screen[])...)
+    s1 = map(handle_drop, parent_screen[].inputs[:dropped_files])
+    position = 1
+    control_s = map(zip(navigation[2], [-1, -1, 1, 1])) do sdir
+        s, dir = sdir
+        map(s, init = nothing) do s
+            position = if s && !isempty(blocklist)
+                for (i, block) in enumerate(blocklist)
+                    position == i ? show!(block) : hide!(block)
+                end
+                step_page(position + dir)
+            else
+                position
+            end
+            _block_position = position
+            nothing
+        end
+    end
+    push!(control_s, s1)
+    push!(navigation, control_s)
+end
+function new_block()
+    global _block_position
+    _block_position += 1
+    hidden = !isempty(blocklist) # only show first block
+    parent = Screen(parent_screen[], hidden = Signal(hidden)) # copy screen, to make it easier to save!
+    editarea, viewarea = x_partition_abs(parent.area, round(Int, 8 * icon_size()))
+    edit_screen = Screen(parent, area = editarea)
+    view_screen = Screen(parent, area = viewarea, name = :GLBook)
+    GLVisualize.add_screen(view_screen) # make screen available
+    edit_screen.stroke = (1, RGBA(0.9f0, 0.9f0, 0.9f0))
+    push!(current_widgetlist, Any[("navigation" => copy(navigation[1]), Signal(0))])
     push!(blocklist, parent)
-    push!(current_widgetlist, :navigation => copy(navigation_visual))
     return view_screen
 end
-
 
 
 function playbutton(name)
     visual, signal = GLVisualize.playbutton(edit_screen())
     signal = map(!, signal)
-    push!(widgetlist(), name => visual)
+    push!(widgetlist(), (name => visual, signal))
     signal
 end
 function playbutton(f, name)
@@ -101,9 +128,11 @@ function playbutton(f, name)
     preserve(map(f, signal))
 end
 
-function slider(range, name; kw_args...)
-    visual, signal = GLVisualize.labeled_slider(range, edit_screen(); kw_args...)
-    push!(widgetlist(), name => visual)
+function slider(range, name; slider_length = 6*icon_size(), kw_args...)
+    visual, signal = GLVisualize.labeled_slider(
+        range, edit_screen(); slider_length = slider_length, kw_args...
+    )
+    push!(widgetlist(), (name => visual, signal))
     signal
 end
 function slider(f::Function, range, name; kw_args...)
@@ -117,28 +146,33 @@ function drawpage(x, style = :default; kw_args...)
 end
 
 
-macro block(window, block)
-    last = gensym()
+macro block(title, block)
+    last, view_screen = gensym(), gensym()
     esc(quote
-        view_screen = GLBooks.new_block()
+        let
+            $view_screen = GLBooks.new_block()
 
 
-        $last = $(block)
+            $last = $(block)
 
-        # TODO make this a GLVisualize function e.g., isvisualizable
-        if applicable(GLVisualize._default, $last, GLAbstraction.Style{:default}(), Dict{Symbol,Any}())
-            GLBooks.drawpage($last)
+            push!(GLBooks.widgetlist(), ("title:" => visualize($title), Signal(0)))
+
+            # TODO make this a GLVisualize function e.g., isvisualizable
+            if applicable(GLVisualize._default, $last, GLAbstraction.Style{:default}(), Dict{Symbol,Any}())
+                GLBooks.drawpage($last)
+            end
+            widgets = convert(Vector{Pair}, map(first, GLBooks.widgetlist()))
+            GLVisualize._view(GLVisualize.visualize(
+                widgets,
+                text_scale = 4*GLVisualize.mm,
+                width = 8*GLBooks.icon_size()
+            ), GLBooks.edit_screen(), camera = :fixed_pixel)
+
+            for cam in keys($view_screen.cameras)
+                GLAbstraction.center!($view_screen, cam) # TODO, don't center corrupt bb's
+            end
+            $last
         end
-        GLVisualize._view(GLVisualize.visualize(
-            GLBooks.widgetlist(),
-            text_scale = 4*GLVisualize.mm,
-            width = 8*GLBooks.icon_size()
-        ), GLBooks.edit_screen(), camera = :fixed_pixel)
-
-        for cam in keys(view_screen.cameras)
-            GLAbstraction.center!(view_screen, cam) # TODO, don't center corrupt bb's
-        end
-        $last
     end)
 end
 
